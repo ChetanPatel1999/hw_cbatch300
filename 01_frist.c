76c616e8b7ad16479f110b683390bb07f6e341db
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-09-07 16:00:55 +0530

    07/09

---
+++ 01_frist.c
@@ -0,0 +1,10 @@
+//this is commnet and ingore by compiler
+//write a programm to display some msg.
+#include<stdio.h>     //stdio.h is a header file which containe defination of printf fuction
+void main()          //void is return type and main is fuction name
+{
+   	printf("hello world institute\n");  // printf a built in fuction which display data on the output screen
+   	printf("this is c code file\n");
+  	printf("this is my frist programm\n");
+}
+
