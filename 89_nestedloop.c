5bedbc3b119357a2f531504ab5bd38ac4456b571
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-10-09 16:13:31 +0530

    09/10

---
+++ 89_nestedloop.c
@@ -0,0 +1,16 @@
+// nested for loop
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++)//2
+    {
+        printf("ab batooo...\n");
+        for (j = 1; j <= 3; j++)//4
+        {
+            printf("hi students ...\n");//15
+        }
+      
+    }
+
+}
---
+++ 90_pattern.c
@@ -0,0 +1,14 @@
+// nested for loop
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)//2
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
